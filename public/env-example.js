6752ee9e2f5df7a0499b37c3556e8f9bfb54811b
John Cosio <mail@jcos.io>
2024-04-23 13:46:14 +0800

    v0.0.0.0

--- public/env-example.js
+++
@@ -1,8 +0,0 @@
-const env = {
-    "API_KEY": "YOUR_API_KEY",
-    "API_URL": "YOUR_API_URL",
-}
-
-export function config() {
-    return env
-}
--- public/releases/v/0100/functions/create/collider.js
+++
@@ -1,5 +0,0 @@
-export function createSetCollidersModule(scene, objects) {
-    scene.physics.add.collider(objects.player, objects.platforms[objects.platformIndex])
-    scene.physics.add.collider(objects.stars, objects.platforms[objects.platformIndex])
-    scene.physics.add.collider(objects.bombs, objects.platforms[objects.platformIndex])
-}
